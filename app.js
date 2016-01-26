6962ee3375d089cf44325bff8302128950ab3adb
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-01-26 12:38:29 -0500

    changes

--- app.js
+++ app.js
@@ -2,13 +2,36 @@
  * Created by haileykeen on 1/25/16.
  */
 var express = require('express');
-var app = express();
 var swig = require('swig');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var wikiRouter = require('./routes/wiki');
+
+var app = express();
+
+var server = app.listen(3000);
 
 morgan.token('id', function getId(req) {
   return req.id
 });
 
 app.use(morgan(':id :method :url :response-time'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
+//app.get('/',function(req, res, next) {
+//  res.render('index');
+//});
+
+// point res.render to the proper directory
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+app.use('/wiki', wikiRouter);
--- models/index.js
+++ models/index.js
@@ -5,18 +5,24 @@ mongoose.connect('mongodb://localhost/wikistack'); // <= db name will be 'wikist
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
+var statuses = ['open', 'closed'];
+
 var pageSchema = new mongoose.Schema({
-  title:    String,
-  urlTitle: String,
-  content:  String,
-  status:   String,
+  title:    {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content:  {type: String, required: true},
+  status:   {type: String, enum: statuses},
   date:     {type: Date, default: Date.now},
   author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
+pageSchema.virtual('route').get(function() {
+  return '/wiki/' + pageSchema.urlTitle;
+});
+
 var userSchema = new mongoose.Schema({
-  name: String,
-  email: String
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true}
 });
 
 var Page = mongoose.model('Page', pageSchema);
